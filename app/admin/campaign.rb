e3b6532f987bd989eb87a783526f0c73ef4dcab9
Prelang Builder <builder@prelang.com>
2015-06-13 01:36:20 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Campaign' to allow for create, update

--- app/admin/campaign.rb
+++ app/admin/campaign.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Campaign do
+  permit_params :name, :description, :active, :case_selector_code
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
