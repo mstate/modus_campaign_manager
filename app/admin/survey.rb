6c29dd275dc92d250d6a9b137d9e6602eab5e365
Prelang Builder <builder@prelang.com>
2015-06-13 01:35:38 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Survey' to allow for create, update

--- app/admin/survey.rb
+++ app/admin/survey.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Survey do
+  permit_params :default_invalid_choice_media, :default_no_choice_media, :default_maximum_repeats_media, :default_maximum_invalid_repeats_media, :default_repeat_question_choices, :default_repeat_options_choices, :default_choice_prompt_repeat_media
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
