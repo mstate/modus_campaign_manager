eb26fe1b15ab6fcbbcbf64193e0f764c95c7295b
Prelang Builder <builder@prelang.com>
2015-06-13 01:35:47 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Question' to allow for create, update

--- app/admin/question.rb
+++ app/admin/question.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Question do
+  permit_params :name, :question_media, :choice_prompt_repeat_media, :no_choice_media, :invalid_choice_media, :maximum_repeats_media, :maximum_invalid_repeats_media, :goto_name, :goto_name_code, :menu_render_options
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
