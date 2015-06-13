15c2a49e2ad40f726d538460c8c1e19d36b1148d
Prelang Builder <builder@prelang.com>
2015-06-13 01:35:27 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'ChoiceSet' to allow for create, update

--- app/admin/choice_set.rb
+++ app/admin/choice_set.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register ChoiceSet do
+  permit_params :choice_prompt_media
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
